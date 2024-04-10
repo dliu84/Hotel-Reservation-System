8f809bf00f13527fff4cfd32185a34fc8e350e8b
刘迪 <mayflower@s-macbook-pro.home>
2024-04-09 20:06:29 -0400

    implemented server

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 	
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -15,11 +19,19 @@ import models.Room;
 import models.RoomManager;
 import models.RoomTypeName;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	private Socket socket;
+	
+	private DataOutputStream dout;
+	private DataInputStream din;
+	
+	private TextArea ta = new TextArea();
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -31,9 +43,29 @@ public class Main extends Application {
 
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			try {
+				socket = new Socket("localhost",8000);
+				din=new DataInputStream(socket.getInputStream());
+				
+				dout = new DataOutputStream(socket.getOutputStream());
+				
+				new Thread(()->run()).start();
+				
+			}catch(IOException ex) {ex.printStackTrace();}
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void run() {
+		try {
+			while(true) {
+				String text = din.readUTF();
+				ta.appendText(text + "\n");
+			}
+		}catch(IOException ex) {ex.printStackTrace();}
 	}
 	
 	public static void main(String[] args) {
